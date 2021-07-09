32890a9ccbafb987d4745aa71680ebdd3a243862
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-07-09 22:44:44 +0900

    feat: add -n flag to echo command

--- cmd/echo.go
+++ cmd/echo.go
@@ -7,6 +7,8 @@ import (
 )
 
 func init() {
+	echoCmd.Flags().BoolP("no-linefeed", "n", false, "no line feed")
+
 	rootCmd.AddCommand(echoCmd)
 }
 
@@ -14,12 +16,15 @@ var echoCmd = &cobra.Command{
 	Use:   "echo",
 	Short: "Echo arguments",
 	Run: func(cmd *cobra.Command, args []string) {
+		nolf, _ := cmd.Flags().GetBool("no-linefeed")
 		for i, arg := range args {
 			fmt.Print(arg)
 			if i != len(args)-1 {
 				fmt.Print(" ")
 			} else {
-				fmt.Print("\n")
+				if !nolf {
+					fmt.Print("\n")
+				}
 			}
 		}
 	},
